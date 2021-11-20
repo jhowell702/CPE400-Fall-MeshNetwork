00b823f5103c8a24142ddae38b7b30571c502093
Justin Howell <justinhowell@nevada.unr.edu>
2021-11-19 16:45:37 -0800

    Add project files.

---
+++ CPE400-Fall-MeshNetwork/inc/graph_node.h
@@ -0,0 +1,9 @@
+class GraphNode {
+
+public:
+
+
+private:
+
+
+};
---
+++ CPE400-Fall-MeshNetwork/src/CPE400-Fall-MeshNetwork.cpp
@@ -0,0 +1,12 @@
+﻿// CPE400-Fall-MeshNetwork.cpp : Defines the entry point for the application.
+//
+
+#include "CPE400-Fall-MeshNetwork.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
